263d217f5341f04207d93495ca1a7e12dfc12e9f
bom <bibidibovidiboo@gmail.com>
2024-01-23 02:36:25 +0900

    Add local storage effect

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,9 +1,25 @@
 import { atom } from 'recoil';
 import { Todo } from 'src/model/types';
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 const todoListState = atom<Todo[]>({
   key: 'todoListState',
   default: [],
+  effects: [localStorageEffect('todo_list')],
 });
 
 export default todoListState;
